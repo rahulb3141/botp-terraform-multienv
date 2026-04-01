51d5d88895e72b91e58c07d0ff8ba606d4a5988a
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-01 10:10:39 +0530

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,1 +1,17 @@
+provider "aws" {
+  region = var.region
+}
 
+module "vpc" {
+  source      = "../../modules/vpc"
+  cidr_block  = var.cidr_block
+  environment = var.environment
+}
+
+module "eks" {
+  source       = "../../modules/eks"
+  cluster_name = "${var.environment}-eks"
+  environment  = var.environment
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.public_subnets
+}
