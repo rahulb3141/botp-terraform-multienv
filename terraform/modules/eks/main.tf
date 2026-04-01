68eb54435e094aabadfa940cda89835ad9b1aea2
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-01 10:05:53 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,1 +1,25 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.15.3"
 
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  manage_aws_auth_configmap = true
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
