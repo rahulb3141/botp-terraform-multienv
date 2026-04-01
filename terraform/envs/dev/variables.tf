c1c1ae7d18510812b063ef95c47073bf4b6e56ab
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-01 10:18:57 +0530

    Create variables.tf

---
+++ terraform/envs/dev/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "cidr_block" {
+  type = string
+  default = "10.0.0.0/16"
+}
