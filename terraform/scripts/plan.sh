8b5b092e2fa9a2cca2b8abc5d903a4a98c627ac6
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-01 10:47:55 +0530

    Update plan.sh

--- terraform/scripts/plan.sh
+++ terraform/scripts/plan.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+set -e
+
+ENV=$1
+
+if [ -z "$ENV" ]; then
+  echo "Usage: ./plan.sh <dev|staging|prod>"
+  exit 1
+fi
+
+echo "📘 Running Terraform PLAN for environment: $ENV"
+
+cd "$(dirname "$0")/../envs/$ENV"
+
+terraform init
+terraform plan -var-file="terraform.tfvars"
