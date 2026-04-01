7700c606853f6e5f629fcf9cec6d5af683a52a06
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-01 10:35:38 +0530

    Update main.tf

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -1,1 +1,27 @@
+terraform {
+  backend "s3" {
+    bucket         = "company-terraform-states"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
 
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source      = "../../modules/vpc"
+  cidr_block  = var.cidr_block
+  environment = var.environment
+}
+
+module "eks" {
+  source       = "../../modules/eks"
+  cluster_name = "${var.environment}-eks"
+  environment  = var.environment
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+}
