82bcc7e7489eb65b925198a8c0c80bd48f37a679
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-02 21:35:52 +0530

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -54,7 +54,7 @@ module "eks" {
   version = "20.24.1"
 
   cluster_name    = "${var.env}-eks"
-  cluster_version = "1.29"
+  cluster_version = "1.35"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
