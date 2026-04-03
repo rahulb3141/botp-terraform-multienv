c70796fd173bf1c78cdf4701f755aebb04c51653
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-03 14:40:50 +0530

    Update main.tf

--- terraform/envs/staging/main.tf
+++ terraform/envs/staging/main.tf
@@ -1,7 +1,16 @@
 terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.40.0, < 6.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "botp-terraform-states-rahul-2026"
-    key            = "prod/terraform.tfstate"
+    key            = "staging/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
@@ -12,16 +21,80 @@ provider "aws" {
 }
 
 module "vpc" {
-  source      = "../../modules/vpc"
-  cidr_block  = var.cidr_block
-  environment = var.environment
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.0"
+
+  name = "${var.env}-vpc"
+  cidr = var.cidr_block
+
+  azs = ["us-east-1a", "us-east-1b"]
+  public_subnets = [
+    cidrsubnet(var.cidr_block, 4, 0),
+    cidrsubnet(var.cidr_block, 4, 1)
+  ]
+
+  map_public_ip_on_launch = true
+
+  private_subnets = [
+    cidrsubnet(var.cidr_block, 4, 2),
+    cidrsubnet(var.cidr_block, 4, 3)
+  ]
+
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = var.env
+  }
 }
 
 module "eks" {
-  source       = "../../modules/eks"
-  cluster_name = "${var.environment}-eks"
-  environment  = var.environment
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.24.1"
+
+  cluster_name    = "${var.env}-eks"
+  cluster_version = "1.31"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
+
+  cluster_endpoint_private_access      = true
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
+  # Enable IRSA
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 1
+      min_size       = 1
+      max_size       = 2
+      instance_types = ["t3.medium"]
+
+      # Essential configurations
+      ami_type      = "AL2_x86_64"
+      capacity_type = "ON_DEMAND"
+
+      # Disk configuration
+      disk_size = 20
+      disk_type = "gp3"
+
+      # Explicitly use your working public subnets
+      subnet_ids = ["subnet-0add2dc0ad8f7c53f", "subnet-0305d2f98ebe15682"]
+
+      # Tagging
+      tags = {
+        Environment = var.env
+        NodeGroup   = "default"
+      }
+    }
+  }
+
+  # Cluster tags
+  tags = {
+    Environment = var.env
+    Terraform   = "true"
+  }
 }
